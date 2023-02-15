780bdff4909f5e3d1f6c8c599b0e5c5056a0300b
johnsolo17 <112775094+johnsolo17@users.noreply.github.com>
2023-02-14 22:18:19 -0500

    commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Smooth Scrolling
+
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
